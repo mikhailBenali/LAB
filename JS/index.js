99dd9ade2fc6aa9e8d17361badb4c113c2c68800
mikhailBenali <94171094+mikhailBenali@users.noreply.github.com>
2023-11-06 11:26:19 -0500

    first commit

---
+++ JS/index.js
@@ -0,0 +1,5 @@
+/*Using the two node.js fs functions: readFile and readFileSync
+Add a text file to be used for reading 
+Console log the output
+Code Tip: https://github.com/anmarjarjees/node.js-start/blob/main/index4.js*/
+
