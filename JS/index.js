227a05178be88933b78579030c07127d4221c4f7
Kazulhu <102474945+Kazulhu@users.noreply.github.com>
2023-11-06 11:36:07 -0500

    Add files via upload

--- JS/index.js
+++ JS/index.js
@@ -1,12 +1,18 @@
 const fs = require('fs');
 
-// Using readFileSync
-const fileContentSync = fs.readFileSync('../LAB/test.txt', 'utf-8');
-console.log(fileContentSync);
+try {
+  // Using readFileSync
+  const fileContentSync = fs.readFileSync('test.txt', 'utf-8');
+  console.log('File Content (Sync):\n', fileContentSync);
+} catch (err) {
+  console.error('Error reading file synchronously:', err);
+}
 
-// Using readFile
-fs.readFile('../LAB/test.txt', 'utf-8', (err, fileContent) => {
-    if (err) throw err;
-    console.log(fileContent);
+// Using readFile with try and catch
+fs.readFile('test.txt', 'utf-8', (err, fileContent) => {
+  try {
+    console.log('File Content (Async):\n', fileContent);
+  } catch (err) {
+    console.error('Error reading file asynchronously:', err);
+  }
 });
-
